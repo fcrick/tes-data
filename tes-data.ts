bebcc89e709dc677c33644edbf194b896d305cd4
Francis Crick <fcrick@gmail.com>
2016-07-07 14:42:12 -0700

    separated out the logic for dealing with the argument being a string or file from the rest
    this is to prepare for more exports having the same flexibility with
    their arguments.

--- tes-data.ts
+++ tes-data.ts
@@ -2,75 +2,93 @@
 
 import fs = require('fs');
 
-export function getRecordOffsets(file: string|number, origOffset: number, callback: (err:NodeJS.ErrnoException, offsets: number[]) => void) {
-  var onOpen = (err: NodeJS.ErrnoException, fd: number) => {
+interface OffsetsCallback {
+  (err:NodeJS.ErrnoException, offsets: number[]): void;
+}
+
+interface ContinuationCallback {
+  (err: NodeJS.ErrnoException, fd: number, callback: OffsetsCallback): void;
+}
+
+function recordOffsets(origOffset: number, err:NodeJS.ErrnoException, fd: number, callback: OffsetsCallback) {
+  // close the file if we finish without errors
+  let success = (err: NodeJS.ErrnoException, offsets: number[]) => {
+    // move this
+    if (typeof fd === 'string') {
+      fs.close(fd, (err: NodeJS.ErrnoException) => {
+        if (err) {
+          callback(err, null);
+        }
+        else {
+          callback(null, offsets);
+        }
+      });
+    }
+    else {
+      callback(null, offsets);
+    }
+  }
+
+  let offsets = [];
+
+  fs.fstat(fd, (err: NodeJS.ErrnoException, stats: fs.Stats) => {
     if (err) {
       callback(err, null);
+      return;
     }
 
-    // close the file if we finish without errors
-    let success = (err: NodeJS.ErrnoException, offsets: number[]) => {
-      if (typeof file === 'string') {
-        fs.close(fd, (err: NodeJS.ErrnoException) => {
-          if (err) {
-            callback(err, null);
-          }
-          else {
-            callback(null, offsets);
-          }
-        });
+    if (stats.size == 0) {
+      success(null, offsets);
+    }
+    else {
+      // if origOffset isn't set, we're reading the whole file at the top level
+      if (!origOffset) {
+        var endOffset = stats.size;
       }
-      else {
-        callback(null, offsets);
+
+      var createRead = (offset: number) => (err: NodeJS.ErrnoException, bytesRead: number, buffer: Buffer) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+
+        var nextOffset = offset + buffer.readUInt32LE(4);
+        if (buffer.toString('utf8', 0, 4) !== 'GRUP') {
+          nextOffset += 24;
+        }
+        else if (!endOffset) {
+          // we just started reading a group, use its size to scope our scan
+          endOffset = nextOffset;
+          nextOffset = offset + 24;
+        }
+
+        if (nextOffset < endOffset) {
+          offsets.push(nextOffset);
+          fs.read(fd, buffer, 0, 8, nextOffset, createRead(nextOffset));
+        }
+        else {
+          success(null, offsets);
+        }
       }
+
+      var buffer = new Buffer(8);
+      fs.read(fd, buffer, 0, 8, origOffset, createRead(origOffset));
+    }
+  });
+}
+
+function handlePathOrFd(file: string | number, continuation: ContinuationCallback, callback: OffsetsCallback) {
+  var onOpen = (err: NodeJS.ErrnoException, fd: number) => {
+    if (err) {
+      continuation(err, null, callback);
     }
 
-    let offsets = [];
+    var withClose: OffsetsCallback = (err, offsets) => {
+      fs.close(fd);
+      callback(err, offsets);
+    }
 
-    fs.fstat(fd, (err: NodeJS.ErrnoException, stats: fs.Stats) => {
-      if (err) {
-        callback(err, null);
-        return;
-      }
-
-      if (stats.size == 0) {
-        success(null, offsets);
-      }
-      else {
-        // if origOffset isn't set, we're reading the whole file at the top level
-        if (!origOffset) {
-          var endOffset = stats.size;
-        }
-
-        var createRead = (offset: number) => (err: NodeJS.ErrnoException, bytesRead: number, buffer: Buffer) => {
-          if (err) {
-            callback(err, null);
-            return;
-          }
-
-          var nextOffset = offset + buffer.readUInt32LE(4);
-          if (buffer.toString('utf8', 0, 4) !== 'GRUP') {
-            nextOffset += 24;
-          }
-          else if (!endOffset) {
-            // we just started reading a group, use its size to scope our scan
-            endOffset = nextOffset;
-            nextOffset = offset + 24;
-          }
-
-          if (nextOffset < endOffset) {
-            offsets.push(nextOffset);
-            fs.read(fd, buffer, 0, 8, nextOffset, createRead(nextOffset));
-          }
-          else {
-            success(null, offsets);
-          }
-        }
-
-        var buffer = new Buffer(8);
-        fs.read(fd, buffer, 0, 8, origOffset, createRead(origOffset));
-      }
-    });
+    continuation(null, fd, callback);
   };
 
   if (typeof file === 'string') {
@@ -80,6 +98,17 @@ export function getRecordOffsets(file: string|number, origOffset: number, callba
     onOpen(null, file);
   }
   else {
-    callback({name:'BadArgument', message:'argument not a string or number'}, null);
+    continuation({name:'BadArgument', message:'argument not a string or number'}, null, callback);
   }
 }
+
+export function getRecordOffsets(file: string|number, origOffset: number, callback: OffsetsCallback) {
+  // make a callback that embeds the arguments we're passing
+  var continuation: ContinuationCallback = (err, fd, callback) => recordOffsets(origOffset, err, fd, callback);
+
+  handlePathOrFd(file, continuation, callback);
+}
+
+// export function parseRecord(file: string|number, origOffset: number, callback: (err:NodeJS.ErrnoException, offsets: number[]) => void) {
+
+// }
