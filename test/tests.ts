d36fdbfd80eb9377fce1e221be998c5396fafdb6
Francis Crick <fcrick@gmail.com>
2016-07-07 15:45:20 -0700

    moved test stuff into functions

--- test/tests.ts
+++ test/tests.ts
@@ -9,57 +9,74 @@ var paths = [
   'HearthFires.esm',
 ];
 
-paths.forEach(filename => {
-  var path = prefix + filename;
-  var logPrefix = filename + ' - ';
+function loadOffsets() {
+  paths.forEach(filename => {
+    var path = prefix + filename;
+    var logPrefix = filename + ' - ';
 
-  console.time(logPrefix + 'find top records');
-  tesData.getRecordOffsets(path, 0, (err: NodeJS.ErrnoException, offsets: number[]) => {
-    //console.log(logPrefix + JSON.stringify(offsets));
-    console.timeEnd(logPrefix + 'find top records');
-  });
+    console.time(logPrefix + 'find top records');
+    tesData.getRecordOffsets(path, 0, (err: NodeJS.ErrnoException, offsets: number[]) => {
+      //console.log(logPrefix + JSON.stringify(offsets));
+      console.timeEnd(logPrefix + 'find top records');
+    });
 
 
-  console.time(logPrefix + 'find all records');
-  fs.open(path, 'r', (err, fd) => {
-    var queue = [0];
-    var seen = new Set<number>();
+    console.time(logPrefix + 'find all records');
+    fs.open(path, 'r', (err, fd) => {
+      var queue = [0];
+      var seen = new Set<number>();
 
-    var readFromQueue = () => {
-      if (queue.length === 0) {
-        fs.close(fd);
-        console.log(logPrefix + seen.size + ' records total');
-        console.timeEnd(logPrefix + 'find all records');
-      }
-
-      var nextSet = queue;
-      queue = [];
-      var remaining = nextSet.length;
-      //console.log(logPrefix + "reading more entries:" + remaining);
-      nextSet.forEach(next => {
-        if (seen.has(next)) {
-          remaining -= 1;
-          if (remaining == 0) {
-            readFromQueue();
-          }
+      var readFromQueue = () => {
+        if (queue.length === 0) {
+          fs.close(fd);
+          console.log(logPrefix + seen.size + ' records total');
+          console.timeEnd(logPrefix + 'find all records');
         }
-        else {
-          seen.add(next);
-          
-          tesData.getRecordOffsets(fd, next, (err, offsets) => {
-            for (var offset of offsets) {
-              queue.push(offset);
-            }
 
+        var nextSet = queue;
+        queue = [];
+        var remaining = nextSet.length;
+        //console.log(logPrefix + "reading more entries:" + remaining);
+        nextSet.forEach(next => {
+          if (seen.has(next)) {
             remaining -= 1;
             if (remaining == 0) {
               readFromQueue();
             }
-          });
-        }
-      });
-    };
+          }
+          else {
+            seen.add(next);
+            
+            tesData.getRecordOffsets(fd, next, (err, offsets) => {
+              for (var offset of offsets) {
+                queue.push(offset);
+              }
 
-    readFromQueue();
+              remaining -= 1;
+              if (remaining == 0) {
+                readFromQueue();
+              }
+            });
+          }
+        });
+      };
+
+      readFromQueue();
+    });
   });
-});
+}
+
+function loadRecords() {
+  var path = prefix + paths[0];
+
+  tesData.getRecordOffsets(path, 0, (err, offsets) => {
+    offsets.forEach(offset => {
+      tesData.getRecord(path, offset, (err, record) => {
+        console.log(offset + ' is ' + JSON.stringify(record));
+      })
+    });
+  });
+}
+
+//loadOffsets();
+loadRecords();
