e972361142b5d434b1c1c9ccd5658f1856f29979
Francis Crick <fcrick@gmail.com>
2016-07-08 19:41:05 -0700

    got subrecords working again in new system

--- record-tes5.ts
+++ record-tes5.ts
@@ -12,45 +12,45 @@ interface RecordField {
 
 export function getRecord(buffer: Buffer): Record {
   var record = <Record>{};
+
+  // read in the header
   readFields(record, buffer, 0, recordHeader);
+
+  // header is always the same size
+  var offset = 24;
+
+  if (offset < buffer.length) {
+    record['subRecords'] = [];
+  }
+
+  var compressed = record.type === 'GRUP' ? false : record['flags'] & 0x40000;
+  if (compressed) {
+    // not yet supported
+    record['compressed'] = true;
+    return record;
+  }
+
+  // read subrecords
+  while (offset < buffer.length) {
+    var subRecord = <Record>{};
+
+    readFields(subRecord, buffer, offset, subRecordFields);
+
+    offset += subRecord.size;
+    record['subRecords'].push(subRecord);
+  }
+
   return record;
 }
 
-export function readFields(record: Record, buffer: Buffer, offset: number, fields: FieldArray): number {
+function readFields(record: Record, buffer: Buffer, offset: number, fields: FieldArray): number {
+  if (!fields) {
+    return offset;
+  }
+
   for (var field of fields) {
     offset = readField(record, buffer, offset, field);
   }
-
-  // if (offset < buffer.length) {
-  //   record['subRecords'] = [];
-  // }
-
-  // var compressed = record.type === 'GRUP' ? false : record['flags'] & 0x40000;
-  // if (compressed) {
-  //   // not yet supported
-  //   record['compressed'] = true;
-  //   return record;
-  // }
-
-  // //var subRecordFields = subRecordMap[record.type];
-
-  // // read subrecords
-  // while (offset < buffer.length) {
-  //   var subRecord: Record = {
-  //     type: buffer.toString('utf8', offset, offset + 4),
-  //     size: buffer.readUInt16LE(offset + 4),
-  //   };
-
-  //   offset += 6;
-  //   var subRecordEnd = offset + subRecord.size;
-
-  //   // if (subRecordFields && subRecord.type in subRecordFields) {
-  //   //   readField(subRecord, buffer, offset, {name:'value', type:subRecordFields[subRecord.type]}, subRecord.size);
-  //   // }
-
-  //   offset += subRecord.size;
-  //   record['subRecords'].push(subRecord);
-  // }
 
   return offset;
 }
@@ -108,6 +108,9 @@ function readField(record: Record, buffer: Buffer, offset: number, field: Field)
 
   var reader = fieldReaders[type];
   if (reader) {
+    if (count === -1) {
+      count = record['size'];
+    }
     var value = reader(buffer, offset, count)
     if (value !== null) {
       record[name] = value;
@@ -118,22 +121,23 @@ function readField(record: Record, buffer: Buffer, offset: number, field: Field)
   return 0;
 }
 
-type FieldType = 'uint32le'|'uint16le'|'uint8'|'char';
+type FieldType = 'uint32le'|'uint16le'|'uint8'|'char'|'byte';
 type SimpleField = [string, FieldType];
 type CountField = [string, number, FieldType];
 type ConditionalFieldSet = [string, {[type:string]:FieldArray}, FieldArray];
+type ConditionalFieldSet2 = [string, {[type:string]:FieldArray}];
 type NestedFieldSet = [string, FieldArray];
-type Field = SimpleField|CountField|ConditionalFieldSet|NestedFieldSet;
+type Field = SimpleField|CountField|ConditionalFieldSet|ConditionalFieldSet2|NestedFieldSet;
 
 // trick to make this being recursive not blow up
 // https://github.com/Microsoft/TypeScript/issues/3496#issuecomment-128553540
 interface FieldArray extends Array<Field> {}
 
 // http://www.uesp.net/wiki/Tes5Mod:Mod_File_Format
-var recordHeader: Field[] = [
+var recordHeader: FieldArray = [
   ['type', 4, 'char'],
   ['size','uint32le'],
-  ['type', {'GRUP': [
+  ['type', {GRUP: [
     ['label', 'uint32le'],
     ['groupType', 'uint32le'],
     ['stamp', 'uint16le'],
@@ -149,6 +153,21 @@ var recordHeader: Field[] = [
   ]],
 ];
 
+var subRecordFields: FieldArray = [
+  ['type', 4, 'char'],
+  ['size', 'uint16le'],
+  ['type', {
+    CNAM: [
+      ['r', 'byte'],
+      ['g', 'byte'],
+      ['b', 'byte'],
+      ['unused', 'byte'],
+    ],
+    EDID: [
+      ['value', -1, 'char'],
+    ],
+  }],
+]
 // var subRecordFields: {[type:string]: FieldType} = {
 //   'CNAM': FieldType.RGB,
 //   'EDID': FieldType.ZString,
