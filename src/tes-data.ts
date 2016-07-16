1849398561a9953eea14917fa802f67a78237cbb
Francis Crick <fcrick@gmail.com>
2016-07-15 20:03:27 -0700

    moved things around

--- src/tes-data.ts
+++ src/tes-data.ts
@@ -1,14 +1,10 @@
 import fs = require('fs');
 
-export interface Callback<T> {
-  (err:NodeJS.ErrnoException, result: T): void;
-}
-
-interface FileContinuation<T> {
-  (fd: number, callback: Callback<T>): void;
-}
-
-function loadRecordBuffer(fd: number, callback: Callback<Buffer>, origOffset: number) {
+export function getRecordBuffer(
+  fd: number,
+  callback: (err: NodeJS.ErrnoException, buffer: Buffer) => void,
+  origOffset: number
+) {
   // first we need to check if this is a group or not, as group records have a fixed size
   var buffer = new Buffer(8);
   fs.read(fd, buffer, 0, 8, origOffset, (err, bytesRead, buffer) => {
@@ -20,45 +16,46 @@ function loadRecordBuffer(fd: number, callback: Callback<Buffer>, origOffset: nu
       callback(null, buffer);
     });
   });
-  
 }
 
-function handlePathOrFd<T>(file: string | number, continuation: FileContinuation<T>, callback: Callback<T>) {
-  var onOpen = (err: NodeJS.ErrnoException, fd: number) => {
-    if (err) {
-      callback(err, null);
-      return;
+export function visit(
+  fd: number,
+  onVisit?: (offset: number, type?: string, parent?: number) => boolean|void,
+  onDone?: (err?: NodeJS.ErrnoException) => void,
+  startOffset?: number,
+  recurse?: boolean
+) {
+  let outstanding = 0;
+  let cancelled = false;
+  startOffset = startOffset || 0;
+  recurse = recurse || typeof recurse === 'undefined' ? true : false;
+
+  var callback: (offset: number, type: string, parent: number) => boolean;
+  callback = (offset, type, parent) => {
+    if (offset !== parent || offset === startOffset) {
+      cancelled = cancelled || <boolean>onVisit(offset, type, parent);
     }
 
-    var withClose: Callback<T> = (err, result) => {
-      // make sure the file is closed if we opened it
-      if (typeof fd === 'string') {
-        fs.close(fd, err => err ? callback(err, null) : callback(null, result));
-      }
-      else {
-        callback(null, result);
-      }
+    if (!cancelled && type === 'GRUP' && offset !== parent && recurse) {
+      outstanding += 1;
+      visitRecordOffsets(fd, offset, callback, done);
     }
 
-    continuation(fd, withClose);
-  };
-
-  if (typeof file === 'string') {
-    fs.open(file, 'r', onOpen);
+    return cancelled;
   }
-  else if (typeof file === 'number') {
-    onOpen(null, file);
-  }
-  else {
-    callback({name:'BadArgument', message:'argument not a string or number'}, null);
-  }
-}
 
-export function getRecordBuffer(file: string|number, origOffset: number, callback: Callback<Buffer>) {
-  // make a callback that embeds the arguments we're passing
-  var continuation: FileContinuation<Buffer> = (fd, callback) => loadRecordBuffer(fd, callback, origOffset);
+  function done(err: NodeJS.ErrnoException) {
+    outstanding -= 1;
+    if (err || outstanding === 0 && onDone) {
+      onDone(err);
 
-  handlePathOrFd(file, continuation, callback);
+      // only report first error
+      onDone = null;
+    }
+  }
+
+  outstanding += 1;
+  visitRecordOffsets(fd, startOffset, callback, done);
 }
 
 function visitRecordOffsets(
@@ -139,44 +136,4 @@ function visitRecordOffsets(
   else {
     fs.fstat(fd, onFstat);
   }
-}
-
-export function visit(
-  fd: number,
-  onVisit?: (offset: number, type?: string, parent?: number) => boolean|void,
-  onDone?: (err?: NodeJS.ErrnoException) => void,
-  startOffset?: number,
-  recurse?: boolean
-) {
-  let outstanding = 0;
-  let cancelled = false;
-  startOffset = startOffset || 0;
-  recurse = recurse || typeof recurse === 'undefined' ? true : false;
-
-  var callback: (offset: number, type: string, parent: number) => boolean;
-  callback = (offset, type, parent) => {
-    if (offset !== parent || offset === startOffset) {
-      cancelled = cancelled || <boolean>onVisit(offset, type, parent);
-    }
-
-    if (!cancelled && type === 'GRUP' && offset !== parent && recurse) {
-      outstanding += 1;
-      visitRecordOffsets(fd, offset, callback, done);
-    }
-
-    return cancelled;
-  }
-
-  function done(err: NodeJS.ErrnoException) {
-    outstanding -= 1;
-    if (err || outstanding === 0 && onDone) {
-      onDone(err);
-
-      // only report first error
-      onDone = null;
-    }
-  }
-
-  outstanding += 1;
-  visitRecordOffsets(fd, startOffset, callback, done);
 }
