2a12a0b2d5fabdf70ee083ab10d5118ac4f8028d
Francis Crick <fcrick@gmail.com>
2016-07-26 09:45:05 -0700

    renaming

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import * as recordTes5 from './record-types';
-import * as tesData from './tes-data'
+import * as visitRecords from './visit-records'
 import * as compression from './compression';
 
 export const getSubRecordOffsets = recordTes5.getSubRecordOffsets;
 export const getRecord = recordTes5.getRecord;
 export const writeRecord = recordTes5.writeRecord;
 
-export const visit = tesData.visit;
+export const visit = visitRecords.visit;
 
 export const deflateRecordBuffer = compression.deflateRecordBuffer;
 export const inflateRecordBuffer = compression.inflateRecordBuffer;
